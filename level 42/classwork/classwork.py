097d2819e8a0349905a158211ae12e3dae170ad3
aleksandre708 <bellatordemios@gmail.com>
2025-02-25 23:31:40 +0400

    commit

---
+++ level 42/classwork/classwork.py
@@ -0,0 +1,19 @@
+dict1 = {
+    "name": "Sandro",
+    "surname": "Shalikashvili",
+    "Academy": "goa",
+    "fav-color": "Green",
+    "City": "Tbilisi",
+    "goa-student": "True",
+    "Age": "16",
+    "fav-programming-lang": "Python",
+}
+print(dict1["name"])
+print(dict1["surname"])
+print(dict1["Academy"])
+print(dict1["fav-color"])
+print(dict1["city"])
+print(dict1["goa-student"])
+print(dict1["age"])
+print(dict1["fav-programming-lang"])
+
